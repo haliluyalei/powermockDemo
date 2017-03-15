eeda7f4e8555da231ce7aa93200ea97981503478
fulei <fulei_master@163.com>
2017-03-15 21:40:09 +0800

    编程规范 修改类名

--- src/runner/SmallTestRunner.java
+++ src/runner/SmallTestRunner.java
@@ -5,22 +5,22 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import demo.mock.finalClass.testFinalClass;
-import demo.mock.helloword.testcaseHelloWorld;
+import demo.mock.helloword.testHelloWorld;
 import demo.mock.localVariable.testLocalVariable;
 import demo.mock.mockConstructors.testConstructor;
 import demo.mock.moreVerify.testMoreVerify;
 import demo.mock.useMatcher.testAnswer;
 import demo.mock.useMatcher.testMatcher;
-import demo.mock.useSpies.testcaseSpies;
+import demo.mock.useSpies.testSpies;
 import demo.mock.useStatic.testStaticClass;
 import fixture.category.Small;
 
 @RunWith(Categories.class)
 @Categories.IncludeCategory(Small.class)
-@Suite.SuiteClasses({testFinalClass.class,testcaseHelloWorld.class,
+@Suite.SuiteClasses({testFinalClass.class,testHelloWorld.class,
 					testLocalVariable.class,testConstructor.class,
 					testMoreVerify.class,testAnswer.class,
-					testMatcher.class,testcaseSpies.class,
+					testMatcher.class,testSpies.class,
 					testStaticClass.class})
 public class SmallTestRunner {
 
